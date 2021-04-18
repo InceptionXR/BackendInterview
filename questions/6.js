fd611d789dded2c20f802421c704cec96cfad27e
Oren Carmel <orenc@inceptionxr.com>
2021-04-18 11:56:40 +0300

    6. Question

---
+++ questions/6.js
@@ -0,0 +1,10 @@
+// 1. Please implement a LinkedList in JavaScript
+
+// 2. Define a maximum size for the linkedlist
+
+// 3. Please create a cache system that can add or remove data as (time) efficiently as possible.
+// the cache should have a maximum size and should delete the oldest record when full.
+
+class LinkedList {
+
+}
